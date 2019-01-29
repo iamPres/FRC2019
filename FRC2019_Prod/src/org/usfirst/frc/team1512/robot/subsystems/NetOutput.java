bfed43bfa3a1bb1bc4408f2cfe2589c91011016a
Preston Willis <prestowillis@gmail.com>
2019-01-28 19:49:47 -0500

    added PID

--- FRC2019_Prod/src/org/usfirst/frc/team1512/robot/subsystems/NetOutput.java
+++ FRC2019_Prod/src/org/usfirst/frc/team1512/robot/subsystems/NetOutput.java
@@ -1,48 +1,46 @@
-package org.usfirst.frc.team1512.robot.commands;
-import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.TimedRobot;
-
 package org.usfirst.frc.team1512.robot.subsystems;
-
-import org.usfirst.frc.team1512.robot.Robot;
-import org.usfirst.frc.team1512.robot.RobotMap;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTableValue;
 
-public class NetOutput extends Subsystem {
-	
-	NetOutput(int user_input, NetworkTableInstance net){
+public class NetOutput extends Subsystem {	
 
+	int input;
+
+	NetOutput(int user_input){
 		// Initialize network tables 
+		NetworkTableInstance net = NetworkTableInstance.getDefault();
 		NetworkTable ball_table = net.getTable("ball_data");
 		NetworkTable tape_table = net.getTable("tape_data");
 
-		public user_input = user_input;
+		input = user_input;
 
 		// Kill all processes and start selected script
-		chooser(user_input);
-		// Wait for startup (ms)
-		wait(500);
+		chooser(input);
+		// Wait for startup 
+		Thread.sleep(500);
 	}
 	
  	public double get_output_of_selected_action() {
-
-		if (user_input == 0) {
+		if (input == 0) {
 			// Return network table value
 			return get_data(ball_table);
 		}
 
-		if (user_input == 1) {	
+		if (input == 1) {	
 			// Return network table value
 			return get_data(tape_table);
 		}
  	}
 
-	public void chooser(int choice){
+	public void chooser(int choice){		
+		// Prototype
 		net.putInt(choice);
+	}
+
+	public double get_data(NetworkTable table){
+		// Prototype
+		return net.getDouble(table);
 	}
  }
