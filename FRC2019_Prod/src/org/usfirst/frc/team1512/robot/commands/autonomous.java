0108e88d8d46c13babc80c4630bc734e5b0eb75e
Preston Willis <prestowillis@gmail.com>
2019-01-28 19:37:29 -0500

    minor changes

--- FRC2019_Prod/src/org/usfirst/frc/team1512/robot/commands/autonomous.java
+++ FRC2019_Prod/src/org/usfirst/frc/team1512/robot/commands/autonomous.java
@@ -2,10 +2,10 @@ package org.usfirst.frc.team1512.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.TimedRobot;
-package org.usfirst.frc.team1512.robot.subsystems;
 
 import org.usfirst.frc.team1512.robot.Robot;
 import org.usfirst.frc.team1512.robot.RobotMap;
+import org.usfirst.frc.team1512.robot.commands.SpinRobotAuto;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
@@ -13,27 +13,21 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTableValue;
 
 public class autonomous extends CommandGroup {
-	NetworkTableValue turn;
 	NetworkTableInstance net = NetworkTableInstance.getDefault();
-	public int error;
-	public int thresh = 1.0;
 	
 	autonomous(){
 		requires(Robot.driveTrain);
 	}
 	
  public autonomous(int choice) {
-	 output = subsystems.NetOutput(choice, net);
+	// Initialize Subsystem Object
+	output = NetOutput(choice, net);
  }
 
  public execute(){
-	 if (error > thresh) {
-	 	PID(output.get_output_of_selected_action());
-	 }
- }
-
- public double PID(){
-	 // PID stuff
+	// Pass network tables output into PID
+	output = output.get_output_of_selected_action();
+	SpinRobotAuto(output);
  }
 
 }
